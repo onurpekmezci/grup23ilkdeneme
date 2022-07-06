ebe576165a2b21424270b6b834be60f6f45c5dea
ŞAFAK SÖZEN <safaksozen@gmail.com>
2022-07-06 21:29:38 +0300

    deneme1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 import java.sql.SQLOutput;
 
 public class Main {
+
     public static void main(String[] args) {
+
         System.out.println("Grup23 ilk çalışma");
+        System.out.println("Safak deneme1 " );
     }
 }
