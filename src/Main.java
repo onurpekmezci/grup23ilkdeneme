5427be83f6a1fe4a9dc8fb9e2cbcb3bec1120eaa
ŞAFAK SÖZEN <safaksozen@gmail.com>
2022-07-06 21:53:56 +0300

    deneme3

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Grup23 ilk çalışma");
-        System.out.println("Safak deneme2" );
+        System.out.println("Safak deneme3" );
     }
 }
