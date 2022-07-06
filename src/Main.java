2ff10e96601c512c641a6c2eebf9c77b9696f3b8
semihsozen <sozensdr1@gmail.com>
2022-07-06 21:56:02 +0300

    degisiklik

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,7 @@ public class Main {
         System.out.println("Grup23 ilk çalışma");
 
         System.out.println("Grup23 yeni çalışma");
+
+        System.out.println("Grup23 ikinci yeni çalışma");
     }
 }
