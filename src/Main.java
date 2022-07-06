c5abe9ed057406dda4b84113c1c60d1791dd39e8
oguzhan <oguzhanmoe@gmail.com>
2022-07-06 21:27:21 +0300

    OguzhanDegisiklik

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Grup23 ilk çalışma");
+
+        System.out.println("OguzhanDegisiklik");
     }
 }
